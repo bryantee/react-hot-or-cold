c06d61e02fb641018aa00eca6cda46f2a39a439c
bryantee <bryantee@gmail.com>
2016-12-28 17:56:03 -0700

    Refactor reducer

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,21 +2,24 @@
 import * as actions from '../actions/index'
 
 const initialState = {
-  randomNumber: null,
+  randomNumber: Math.floor(Math.random() * 100),
   guesses: [],
-  correctGuess: false
+  correctGuess: false,
+  message: ''
 };
 
-const quizReducer = (state=initialState, action) => {
+const quizReducer = (state, action) => {
+  if (!state) state = {...initialState}
+  console.log(`Random number: ${state.randomNumber}`);
   switch (action.type) {
     case actions.SET_RAND_NUM:
       return {...state, randomNumber: action.randomNumber };
     case actions.NEW_GAME:
-      return {...initialState};
+      return {...initialState, randomNumber: Math.floor(Math.random() * 100)};
     case actions.NEW_GUESS:
       let guesses = state.guesses.concat(action.userGuess);
       let correctGuess = false;
-      if (state.randomNumber === action.userGuess) correctGuess = true;
+      if (state.randomNumber === parseInt(action.userGuess)) correctGuess = true;
       return {...state, guesses: guesses, correctGuess: correctGuess};
   }
   return state;
