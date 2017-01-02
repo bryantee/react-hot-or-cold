5c71259edbd1b4f7f3efb2a437b629adf979817f
bryantee <bryantee@gmail.com>
2017-01-01 17:40:43 -0700

    Setup backend to support fewest-guesses API
    resolves issue #2

---
+++ server.js
@@ -0,0 +1,27 @@
+'use static';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(express.static('build'));
+app.use(bodyParser.json());
+
+let fewestGuesses = 'No one has one yet';
+
+app.get('/fewest-guesses', (req, res) => {
+  res.status(200).json({ fewestGuesses });
+});
+
+app.post('/fewest-guesses', (req, res) => {
+  if (typeof fewestGuesses == 'string' || req.body.numberOfGuesses < fewestGuesses) {
+    fewestGuesses = req.body.numberOfGuesses;
+    res.status(200).json({ message: 'You are now have the fewest guesses!', fewestGuesses});
+  } else {
+    res.status(200).json({ message: 'Sorry, you didn\'t get the fewest guesses. Try again.'});
+  }
+});
+
+app.listen(8080,  () => {
+  console.log('Listening on port 8080');
+})
